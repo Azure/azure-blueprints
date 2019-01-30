600669665c0b2ba679fbf95e90214b8c47deb206
ajf214 <alex.j.frankel@gmail.com>
2019-01-30 09:12:31 -0800

    adding ps scripts for jorge

---
+++ VDCArtifacts/group_policy_settings.ps1
@@ -0,0 +1,22 @@
+﻿param(
+    [Parameter(Mandatory = $True)]
+    [Int]$MaxPwdAge,
+
+    [Parameter(Mandatory = $True)]
+    [Int]$MinPwdAge,
+
+    [Parameter(Mandatory = $True)]
+    [Int]$MinPwdLength,
+
+    [Parameter(Mandatory = $True)]
+    [Int]$PwdHistoryCount,
+
+    [Parameter(Mandatory = $True)]
+    [String]$Identity
+) 
+
+$maximumPwdAge = New-TimeSpan -Days $MaxPwdAge
+$minimumPwdAge = New-TimeSpan -Days $MinPwdAge
+
+
+Set-ADDefaultDomainPasswordPolicy -ComplexityEnabled $true -MaxPasswordAge $maximumPwdAge -MinPasswordAge $minimumPwdAge -MinPasswordLength $MinPwdLength -PasswordHistoryCount $PwdHistoryCount -Identity $Identity
---
+++ VDCArtifacts/local_policy_settings.ps1
@@ -0,0 +1,17 @@
+﻿param(
+    [Parameter(Mandatory = $True)]
+    [Int]$MaxPwdAge,
+
+    [Parameter(Mandatory = $True)]
+    [Int]$MinPwdAge,
+
+    [Parameter(Mandatory = $True)]
+    [Int]$MinPwdLength,
+
+    [Parameter(Mandatory = $True)]
+    [Int]$PwdHistoryCount
+) 
+net accounts /minpwlen:$MinPwdLength
+net accounts /maxpwage:$MaxPwdAge
+net accounts /minpwage:$MinPwdAge
+net accounts /uniquepw:$PwdHistoryCount

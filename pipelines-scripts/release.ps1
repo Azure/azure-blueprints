0cb083e80c0a29f35330f8dc698f469b4dc19c6a
Alex Frankel <alfran@microsoft.com>
2019-12-09 15:45:47 -0800

    resolving a few outstanding PRs - don't install module if it already exists, replace blueprintId dynamically, made assignment creation idempotent

--- pipelines-scripts/release.ps1
+++ pipelines-scripts/release.ps1
@@ -1,3 +1,6 @@
+# Sample usage
+# .\pipelines-scripts\release.ps1 -mgId "2cc4a1e3-2d9e-4d60-9f42-43da6960ac91" -blueprintName 101-boilerplate -spnId "1476502b-28c9-4b48-badc-4f5c2b74f361" -spnPass "" -tenantId "72f988bf-86f1-41af-91ab-2d7cd011db47" -parametersPath .\samples\101-boilerplate\assign.json -subscriptionId "e93d3ee6-fac1-412f-92d6-bfb379e81af2"
+
 param(
     [Parameter(Mandatory=$true)]$mgId, 
     [Parameter(Mandatory=$true)]$blueprintName,
@@ -11,9 +14,13 @@ param(
 # Output powershell version for debugging purposes and is probably generally good to know
 $PSVersionTable.PSVersion # Assuming powershell core (6)
 
-Write-Host "Installing Az module" # todo - should check if blueprint module is already installed
-Install-Module -Name Az.Blueprint -AllowClobber
-Write-Host "Successfully installed Az.Blueprint module"
+if (!(Get-Module -ListAvailable -Name Az.Blueprint)) {
+    Write-Host "Installing Az module"
+    Install-Module -Name Az.Blueprint -AllowClobber
+}else
+{
+    Write-Host "Az.Blueprint module already exists"
+}
 
 Write-Host "Start login with SPN"
 $pass = ConvertTo-SecureString $spnPass -AsPlainText -Force
@@ -23,14 +30,21 @@ Login-AzAccount -Credential $cred -ServicePrincipal -TenantId $tenantId
 Write-Host "Azure context:"
 Get-AzContext
 
-$importedBp = Get-AzBlueprint -ManagementGroupId $mgId -Name $blueprintName -LatestPublished
+$latestPublishedBp = Get-AzBlueprint -ManagementGroupId $mgId -Name $blueprintName -LatestPublished
 
 # Auto-inserts blueprintId into parameters file
 $content = Get-Content $parametersPath -raw | ConvertFrom-Json
-$content.properties | % {if($_.blueprintId -ne $importedBp.id){$_.blueprintId=$importedBp.id}}
+$content.properties | % {if($_.blueprintId -ne $latestPublishedBp.id){$_.blueprintId=$latestPublishedBp.id}}
 $content | ConvertTo-Json -Depth 100| set-content $parametersPath
 
-New-AzBlueprintAssignment -Name "pla-$blueprintName" -Blueprint $importedBp -AssignmentFile $parametersPath -SubscriptionId $subscriptionId 
+$existingBp = Get-AzBlueprintAssignment -Name "pla-$blueprintName" -subscriptionId $subscriptionId -ErrorAction SilentlyContinue
+
+if ($existingBp) {
+    Set-AzBlueprintAssignment -Name "pla-$blueprintName" -Blueprint $latestPublishedBp -AssignmentFile $parametersPath -SubscriptionId $subscriptionId 
+} else {
+    Write-Host "No existing blueprint assignment. Creating one..."
+    New-AzBlueprintAssignment -Name "pla-$blueprintName" -Blueprint $latestPublishedBp -AssignmentFile $parametersPath -SubscriptionId $subscriptionId
+} 
 
 # Wait for assignment to complete
 $timeout = new-timespan -Seconds 500
@@ -50,7 +64,8 @@ if ($AssignemntStatus.ProvisioningState -ne "Succeeded") {
 
 # publish 'stable' version
 $date = Get-Date -UFormat %Y%m%d.%H%M%S
-$genVersion = "$date.STABLE" # todo - use the version from DevOps
-Publish-AzBlueprint -Blueprint $importedBp -Version $genVersion
+$genVersion = "$date.STBL" # todo - use the version from DevOps
+$blueprint = Get-AzBlueprint -ManagementGroupId $mgId -Name $blueprintName
+Publish-AzBlueprint -Blueprint $blueprint -Version $genVersion
 
 
